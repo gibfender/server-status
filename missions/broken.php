605428ccec6d65ad1868427ed03bcebc179d174b
gibfender <tom.pandorasbox@gmail.com>
2017-06-30 19:03:27 -0400

    Changed broken.php to use PDO properly, added comment framework

--- missions/broken.php
+++ missions/broken.php
@@ -4,13 +4,24 @@ require '../settings.php';
 
 rename($missionsdir.$_POST['filename'], $brokendir.$_POST['filename']);
 
-if(isset($_POST['id'])) {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", "$username", "$password");
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->prepare("UPDATE missions SET broken='1' WHERE id=('".$_POST['id']."')");
-  $stmt->execute();
-  $conn = null;
-}
+        $id = $_POST['id'];
+        $filename = $_POST['filename'];
+        $brokentype = $_POST['brokentype'];
+        $brokendes = $_POST['brokendes'];
 
+
+        $dsn = "mysql:host=$servername;dbname=$dbname;";
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION
+          ];
+        $pdo = new PDO($dsn, $username, $password, $opt);
+        $sql = "UPDATE missions SET
+                                    dateupdated = CURDATE(),
+                                    broken = ?,
+                                    brokentype = ?,
+                                    brokendes = ?
+                                    WHERE id = '$id'";
+        $pdo->prepare($sql)->execute([1,$brokentype,$brokendes]);
+        header('Location: /mission.php?id='.$id);
 
 ?>
